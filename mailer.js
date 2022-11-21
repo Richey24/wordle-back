85c62911cb67fc8ec9b7fa451bfce716cd1b46ce
Richey24 <uahomorejoice@gmail.com>
2022-11-21 19:41:52 +0100

    changes

--- mailer.js
+++ mailer.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config({ path: ".env" })
 
-const sendMail = async (email, id) => {
+const sendMail = async (email, id, username) => {
     let transporter = nodemailer.createTransport({
         service: "Outlook365",
         auth: {
@@ -14,8 +14,18 @@ const sendMail = async (email, id) => {
         to: `${email}, ${email}`,
         subject: "Reset Your Password",
         html: `
-            <p>Follow this link to reset your password</p>
-            <a href="localhost:3000/reset/${id}">Reset</a>
+            <h4>Hello ${username}</h4>
+            <br>
+            <p><b>A request has been received to change the password for your account</b></p>
+            <br>
+            <button style="border: none; outline: none; width: 100px; height: 50px; background-color: blue; color: white">
+            <a style="color: white; text-decoration: none" href="http://localhost:3000/reset/${id}">Reset</a>
+            </button>
+            <br>
+            <p>If you did not initiate this request, please ignore this email</p>
+            <br>
+            <p>Thank you</p>
+            <p>The Bible Camp team</p>
         `
     })
 }
