8f03219627d98e267e9e658986f54c7e0c37caae
Richey24 <uahomorejoice@gmail.com>
2023-01-27 03:20:43 +0100

    changes

--- confirm.js
+++ confirm.js
@@ -15,11 +15,11 @@ const confirmMail = async (email, id, name) => {
         subject: "Confirm your account",
         html: `
             <p>Hello ${name},</p>
-            <p>Thanks for registering your account click the link below to verify your account and login</p>
+            <p>Thanks for registering your account click the link below to verify your account and login.</p>
             <a style="color: blue; text-decoration: none" href="https://www.israelbiblecamp.world/verify/${id}">Verify</a>
-            <p>If you did not initiate this request, please ignore this email</p>
+            <p>If you did not initiate this request, please ignore this email.</p>
             <p>Thank you!</p>
-            <p>The Bible Camp team</p>
+            <p>The Bible Camp team.</p>
         `
     })
 }
--- mailer.js
+++ mailer.js
@@ -15,11 +15,11 @@ const sendMail = async (email, id, name) => {
         subject: "Reset Your Password",
         html: `
             <p>Hello ${name},</p>
-            <p>A request has been received to change the password for your account</p>
+            <p>A request has been received to change the password for your account.</p>
             <a style="color: blue; text-decoration: none" href="https://www.israelbiblecamp.world/reset/${id}">Reset</a>
-            <p>If you did not initiate this request, please ignore this email</p>
+            <p>If you did not initiate this request, please ignore this email.</p>
             <p>Thank you!</p>
-            <p>The Bible Camp team</p>
+            <p>The Bible Camp team.</p>
         `
     })
 }
