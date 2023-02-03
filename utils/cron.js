b6afd21b5fcf995863bb2a52af6fca022ad15702
Richey24 <uahomorejoice@gmail.com>
2023-02-03 14:02:57 +0100

    updated cron job to weekly, created webhook, split name schema to first and last

--- utils/cron.js
+++ utils/cron.js
@@ -8,11 +8,16 @@ const resetScore = () => {
             await User.findByIdAndUpdate(user._id, { dailyWQS: 0 })
             await User.findByIdAndUpdate(user._id, { dailyBQS: 0 })
             await User.findByIdAndUpdate(user._id, { dailyHS: 0 })
+        })
+        await Score.deleteMany({})
+    })
+    cron.schedule("0 0 * * *", async () => {
+        const users = await User.find({})
+        users.forEach(async (user) => {
             await User.findByIdAndUpdate(user._id, { playedHang: false })
             await User.findByIdAndUpdate(user._id, { playedTrivial: false })
             await User.findByIdAndUpdate(user._id, { playedBible: false })
         })
-        await Score.deleteMany({})
     })
 }
 
