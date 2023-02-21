44949b6ad7fd389a08b0e2b55e3e51fc91b6e098
Richey24 <uahomorejoice@gmail.com>
2023-02-21 01:08:58 +0100

    added cancel subscription

--- mail/cancelMail.js
+++ mail/cancelMail.js
@@ -8,7 +8,7 @@ const cancelMail = async (email, id, name) => {
         html: `
             <p>Hello ${name},</p>
             <p>You requested to cancel your subscription, you will lose access to all our premium features once your current subscription expires.</p>
-            <a style="color: blue; text-decoration: none" href="https://www.israelbiblecamp.world/cancel/sub/${id}">Verify</a>
+            <a style="color: blue; text-decoration: none" href="https://www.israelbiblecamp.world/cancel/sub/${id}">Cancel</a>
             <p>If you did not initiate this request, please ignore this email.</p>
             <p>Thank you!</p>
             <p>The Bible Camp team.</p>
