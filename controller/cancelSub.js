1adee52fa0fc933bb60520732e51c4a434350776
Richey24 <uahomorejoice@gmail.com>
2023-02-21 02:01:03 +0100

    added cancel subscription

--- controller/cancelSub.js
+++ controller/cancelSub.js
@@ -1,13 +1,16 @@
 const cancelMail = require("../mail/cancelMail")
 
 const cancelSub = async (req, res) => {
-    const { email, id, firstName } = req.body
-    if (!email) {
-        return res.status(400).json({ message: "Email is required" })
+    try {
+        const { email, id, firstName } = req.body
+        if (!email) {
+            return res.status(400).json({ message: "Email is required" })
+        }
+        cancelMail(email, id, firstName)
+        res.status(200).json({ message: "Mail sent successfully" })
+    } catch (error) {
+        res.status(500).json({ message: "An error occurred" })
     }
-    cancelMail(email, id, firstName)
-    res.status(200).json({ message: "Mail sent successfully" })
-
 }
 
 module.exports = cancelSub
