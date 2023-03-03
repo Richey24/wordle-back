6169bb274f3c2e0657a3f1cd4355a6d645e9f337
Richey24 <uahomorejoice@gmail.com>
2023-03-03 17:47:52 +0100

    fixes

--- routes/deckRoutes.js
+++ routes/deckRoutes.js
@@ -3,17 +3,18 @@ const addDeck = require('../deckController/addDeck');
 const getAllDeck = require('../deckController/getAllDeck');
 const deckRoutes = express.Router()
 
-const restrict = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(" ")[1];
-        await promisify(jwt.verify)(token, "rich")
-        next()
-    } catch (error) {
-        return res.status(401).json({ message: "invalid token" })
-    }
-}
+// const restrict = async (req, res, next) => {
+//     try {
+//         const token = req.headers.authorization.split(" ")[1];
+//         console.log(token);
+//         await promisify(jwt.verify)(token, "rich")
+//         next()
+//     } catch (error) {
+//         return res.status(401).json({ message: "invalid token" })
+//     }
+// }
 
-deckRoutes.post("/create", restrict, addDeck)
-deckRoutes.get("/get/all", restrict, getAllDeck)
+deckRoutes.post("/create", addDeck)
+deckRoutes.get("/get/all", getAllDeck)
 
 module.exports = deckRoutes
